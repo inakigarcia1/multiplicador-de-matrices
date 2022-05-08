0395714cf324b5f95f569615e07c58baf0de9bf5
inakigarcia1 <inakigarciatarcic@gmail.com>
2022-05-07 21:33:10 -0300

    Calculadora funcional para multiplicar solamente 2 matrices.

--- matrices.py
+++ matrices.py
@@ -15,7 +15,7 @@ for i in range(cantidad + 1):
 
 matrices = {}
 elementos = {}
-i = 0
+letra = 0
 letras = 'abcdefghijk'
 for fila, columna in orden:
     for numero_fila in range(fila + 1):
@@ -24,23 +24,48 @@ for fila, columna in orden:
         for numero_columna in range(columna + 1):
             if numero_columna == 0:
                 continue
-            elementos[letras[i] + str(numero_fila) + str(numero_columna)] = input(f'Ingrese el elemento {letras[i]}{numero_fila}{numero_columna}: ')
-            elementos[letras[i] + str(numero_fila) + str(numero_columna)] = int(elementos[letras[i] + str(numero_fila) + str(numero_columna)])
-    matrices[letras[i]] = elementos
+            elementos[letras[letra] + str(numero_fila) + str(numero_columna)] = input(
+                f'Ingrese el elemento {letras[letra]}{numero_fila}{numero_columna}: ')
+            elementos[letras[letra] + str(numero_fila) + str(numero_columna)] = int(
+                elementos[letras[letra] + str(numero_fila) + str(numero_columna)])
+    matrices[letras[letra]] = elementos
     elementos = {}
-    i = i + 1
+    letra = letra + 1
 
-print(matrices)
-print(matrices['a'])
-
-# resultado = {}
+matriz_resultado = {}
 
 
-# for fila, columna in orden:
-#     for numero_fila in range(fila + 1):
-#         if numero_fila == 0:
-#             continue
-#         for numero_columna in range(columna + 1):
-#             if numero_columna == 0:
-#                 continue
-#             resultado[letras[i + 1] + str(numero_fila) + str(numero_columna)] = elementos['a11'] * elementos['b11']
+def calcular_producto(contador, filac, columnac):
+    global matriz
+    global matriz2
+    global matriz_a
+    global matriz_b
+    resultado = 0
+    for contador in range(contador + 1):
+        if contador == 0:
+            continue
+        resultado = resultado + (
+                matriz_a[matriz + str(filac) + str(contador)] * matriz_b[matriz2 + str(contador) + str(columnac)])
+    return resultado
+
+
+for i in range(cantidad - 1):
+    matriz = letras[i]
+    matriz2 = letras[i + 1]
+    matriz_a = matrices[matriz]
+    matriz_b = matrices[matriz2]
+    orden_a = orden[i]
+    orden_b = orden[i + 1]
+    orden_resultado = (orden_a[0], orden_b[1])
+
+    for numero_fila in range(orden_resultado[0] + 1):
+        if numero_fila == 0:
+            continue
+        for numero_columna in range(orden_resultado[1] + 1):
+            if numero_columna == 0:
+                continue
+            elementos[letras[letra] + str(numero_fila) + str(numero_columna)] = calcular_producto(orden_a[1],
+                                                                                                  numero_fila,
+                                                                                                  numero_columna)
+
+    print(elementos)
